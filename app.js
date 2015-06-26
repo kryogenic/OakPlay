bde5e3ff78e4740e25c2abc01218db97c075dc4a
kryo <krydrogen@gmail.com>
2015-06-25 21:39:18 -0700

    some code for creating/deleting bookings

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ require('./database/generate');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var timetable = require('./routes/timetable');
+var bookings = require('./routes/bookings');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/timetable', timetable);
+app.use('/booking/', bookings)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/bookings.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Booking = require('../models/booking');
+
+router.post('/:booking_id/delete', function(req, res, next) {
+    Booking.remove({_id:req.body.booking_id});
+});
+
+router.post('/create', function(req, res, next) {
+    // todo validate day/timeslot/facility, get the current user, and handle errors when saving
+    var b = new Booking({
+        day: req.body.day,
+        timeslot: req.body.timeslot,
+        facility: req.body.facility,
+        user: null
+    });
+    b.save();
+});
