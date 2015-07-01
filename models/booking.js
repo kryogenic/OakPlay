40cfcc92c1236fdb3728a258ef33b197d4a55d14
jwrg <jwrg@users.noreply.github.com>
2015-06-30 23:34:23 -0700

    Altered models to reflect class model in architecture specification

--- models/booking.js
+++ models/booking.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 
 var BookingSchema = new mongoose.Schema({
-    day: { type: String, required: true, index: true },
-    timeslot: { type: [Number], required: true, index: true },
+    day:      { type: String, required: true, index: true },
+    res_id:   { type: Number, required: true, index: { unique: true } },
+    timeslot: { type: Number, required: true, index: true },
+    duration: { type: Number, required: true, index: true },
     facility: { type: mongoose.Schema.ObjectId, ref: 'Facility', required: true, index: true },
-    user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true, index: true }
+    user:     { type: mongoose.Schema.ObjectId, ref: 'User', required: true, index: true }
 });
 
 module.exports = mongoose.model('Booking', BookingSchema);
--- models/facility.js
+++ models/facility.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 var FacilitySchema = new mongoose.Schema({
-    name: { type: String, required: true }
+    name:         { type: String, required: true, index: true },
+    id:           { type: Number, required: true, index: true },
+    capacity:     { type: Number, required: true, index: true },
+    description:  { type: String, required: false }
 })
 
 module.exports = mongoose.model('Facility', FacilitySchema);
