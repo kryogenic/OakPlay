08787e3871f7cd5c4c9b44c6fb6c3b928079f3e0
kryo <krydrogen@gmail.com>
2015-07-08 18:30:15 -0700

    move inline js into timetable javascript file

--- public/javascripts/timetable.js
+++ public/javascripts/timetable.js
@@ -12,3 +12,19 @@ $(document).ready(function(){
         });
     });
 });
+
+$(".available-time").click(function(e){
+    var timeslotDiv = $(this);
+    $.post("/bookings/create", {
+        day: timeslotDiv.attr('data-day'),
+        timeslot: timeslotDiv.attr('data-time'),
+        facility:facility_id})
+        .done(function(data) {
+            if(data.success) {
+                timeslotDiv.removeClass("available-time");
+                timeslotDiv.addClass("userbooked-time");
+            } else {
+                alert(data.message);
+            }
+        });
+});
